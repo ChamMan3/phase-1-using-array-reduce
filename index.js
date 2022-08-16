79506b823a3edd8479aa76c2862890df26dc1abf
LukeChamley <lkchamley@gmail.com>
2022-08-16 14:05:20 -0600

    commit tree 3c713c47f872789200ac3a3f8d5502a996320134

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-
 // Code your solution here
+
+// const totalBatteries = batteryBatches.reduce(function(batteryBatches, currentBat) {
+//     if (currentBat in batteryBatches) {
+//         batteryBatches[currentBat]++;
+//     } else {
+//         batteryBatches[currentBat] = 1;
+//     }
+//     return batteryBatches;
+//   },
+// );
+// function reducer(accumulator, batteryBatches){
+//     for(let bat in batteryBatches){
+//         bat
+//     }
+//     // return accumulator += 
+// }
+
+// //let totalBatteries = batteryBatches.reduce(reducer,0);
+
+const totalBatteries = batteryBatches.reduce(function(total, batches){
+    return batches + total
+    }
+  )
